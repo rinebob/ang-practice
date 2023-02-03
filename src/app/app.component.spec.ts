578f907bfb2d7fd6f3fb7b3942aba16ccd6fb055
rinebob <rinebob111185@gmail.com>
2023-02-03 10:39:10 -0800

    Initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HarnessLoader } from '@angular/cdk/testing';
 import {TestbedHarnessEnvironment} from '@angular/cdk/testing/testbed';
@@ -16,12 +17,23 @@ describe('AppComponent', () => {
     await TestBed.configureTestingModule({
       imports: [
         AppModule,
+=======
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+>>>>>>> 1a7b65d (Initial commit)
         RouterTestingModule
       ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
+<<<<<<< HEAD
 
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
@@ -48,4 +60,26 @@ describe('AppComponent', () => {
     expect(bottomSection).toBeTruthy();
   });
 
+=======
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'ang-practice'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('ang-practice');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ang-practice app is running!');
+  });
+>>>>>>> 1a7b65d (Initial commit)
 });
