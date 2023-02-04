c4ee7ba9eea2748f1b510efcfb214cbe0ba9f1e9
rinebob <rinebob111185@gmail.com>
2023-02-03 16:54:16 -0800

    Implemented UI treatments for Action buttons
    --Styles, before pseudo element, hover, gradients

---
+++ src/app/action-buttons/action-buttons.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'rb-action-buttons',
+  templateUrl: './action-buttons.component.html',
+  styleUrls: ['./action-buttons.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ActionButtonsComponent {
+
+}
