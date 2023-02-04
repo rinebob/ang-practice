856fa215f717932c1b30e7db76b8aaabf32500b8
rinebob <rinebob111185@gmail.com>
2023-02-04 11:00:40 -0800

    Scaffolding for BookmarkTile component

---
+++ src/app/bookmark-tile/bookmark-tile.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'rb-bookmark-tile',
+  templateUrl: './bookmark-tile.component.html',
+  styleUrls: ['./bookmark-tile.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class BookmarkTileComponent {
+
+}
