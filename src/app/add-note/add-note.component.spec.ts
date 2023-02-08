82f118f4873ef831f45c537bda66ce4e36c2d27d
rinebob <rinebob111185@gmail.com>
2023-02-08 15:32:57 -0800

    Added validation and tests for AddNote component and form

--- src/app/add-note/add-note.component.spec.ts
+++ src/app/add-note/add-note.component.spec.ts
@@ -3,36 +3,43 @@ import { HarnessLoader } from '@angular/cdk/testing';
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { AddNoteHarness } from './testing/add-note-harness';
 
+import { NoteService } from '../services/note.service';
 import { AppModule } from '../app.module';
 import { AddNoteComponent } from './add-note.component';
 
-describe('AddNoteComponent', () => {
+fdescribe('AddNoteComponent', () => {
   let component: AddNoteComponent;
   let fixture: ComponentFixture<AddNoteComponent>;
-  let loader: HarnessLoader;
   let harness: AddNoteHarness;
+  let fakeNotesService: Partial<NoteService>;
 
   beforeEach(async () => {
+    fakeNotesService = jasmine.createSpyObj<NoteService>('NoteService', [
+      'createNote'
+    ]);
+
     await TestBed.configureTestingModule({
-      imports: [AppModule],
       declarations: [ AddNoteComponent ],
+      imports: [AppModule],
+      providers: [
+        {provide: NoteService, useValue: fakeNotesService},
+      ],
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(AddNoteComponent);
     component = fixture.componentInstance;
     harness = await TestbedHarnessEnvironment.harnessForFixture<AddNoteHarness>(fixture, AddNoteHarness);
-    loader = TestbedHarnessEnvironment.loader(fixture);
     fixture.detectChanges();
   });
 
   it('should be created and have all elements', async () => {
     const note = await harness.getNote();
-    const form = await harness.getElement('note-form');
-    const titleInput = await harness.getElement('title-input');
-    const textInput = await harness.getElement('note-textarea');
-    const createButton = await harness.getElement('create-button');
-    const cancelButton = await harness.getElement('cancel-button');
+    const form = await harness.getForm();
+    const titleInput = await harness.getTitle();
+    const textInput = await harness.getContent();
+    const createButton = await harness.getCreateButton();
+    const cancelButton = await harness.getCancelButton();
     
     expect(component).toBeTruthy();
     expect(note).toBeTruthy();
@@ -41,5 +48,106 @@ describe('AddNoteComponent', () => {
     expect(textInput).toBeTruthy();
     expect(createButton).toBeTruthy();
     expect(cancelButton).toBeTruthy();
+  });
+
+  it('create button should be disabled when form instantiated', async () => {
+    const createButton = await harness.getCreateButton();
+    
+    expect(await createButton.hasClass('disabled')).toBe(true);
+  });
+
+  it('form should be invalid and have required errors', async () => {
+    const form = component.noteForm;
+    const titleControl = form.get('titleControl');
+    const contentControl = form.get('contentControl');
+
+    expect(form.status).toEqual('INVALID');
+    expect(titleControl?.status).toBe('INVALID');
+    expect(titleControl?.errors?.['required']).toBe(true);
+    expect(contentControl?.status).toBe('INVALID');
+    expect(contentControl?.errors?.['required']).toBe(true);
+  });
+
+  it('entering title removes title control errors', async () => {
+    const form = component.noteForm;
+    const titleControl = form.get('titleControl');
+    
+    expect(titleControl?.value).toEqual('');
+    
+    titleControl?.setValue('New title');
+    
+    expect(titleControl?.value).toEqual('New title');
+    expect(titleControl?.status).toBe('VALID');
+    expect(titleControl?.errors).toBe(null);
+    expect(form.status).toBe('INVALID');
+  });
+
+  it('entering content removes content control errors', async () => {
+    const form = component.noteForm;
+    const contentControl = form.get('contentControl');
+    
+    expect(contentControl?.value).toEqual('');
+    
+    contentControl?.setValue('New content');
+    
+    expect(contentControl?.value).toEqual('New content');
+    expect(contentControl?.status).toBe('VALID');
+    expect(contentControl?.errors).toBe(null);
+    expect(form.status).toBe('INVALID');
+  });
+
+  it('entering all data makes form valid', async () => {
+    const form = component.noteForm;
+    const titleControl = form.get('titleControl');
+    const contentControl = form.get('contentControl');
+    
+    expect(titleControl?.value).toEqual('');
+    expect(contentControl?.value).toEqual('');
+    
+    titleControl?.setValue('Different title');
+    contentControl?.setValue('Different content');
+
+    expect(titleControl?.value).toEqual('Different title');
+    expect(contentControl?.value).toEqual('Different content');
+    expect(titleControl?.status).toBe('VALID');
+    expect(titleControl?.errors).toBe(null);
+    expect(contentControl?.status).toBe('VALID');
+    expect(contentControl?.errors).toBe(null);
+    expect(form.status).toBe('VALID');
+    expect(form.errors).toBe(null);
+    
+  });
+
+  it('submit button works when form is valid', async () => {
+    spyOn(component, 'handleFormSubmit').and.callThrough();
+    const form = component.noteForm;
+    const titleElement = await harness.getTitle();
+    const contentElement = await harness.getContent();
+    const createButton = await harness.createButton();
+   
+    titleElement.setInputValue('Different title');
+    titleElement.dispatchEvent('input');
+    contentElement.setInputValue('Different content');
+    contentElement.dispatchEvent('input');
+    fixture.detectChanges();
+   
+    const note = {
+      title: form.get('titleControl')?.value,
+      content: form.get('contentControl')?.value,
+    };
+
+    await createButton.click();
+
+    expect(component.handleFormSubmit).toHaveBeenCalled()
+    expect(fakeNotesService.createNote).toHaveBeenCalledWith(note)
+  });
+
+  it('cancel button navigates back to Notes component', async () => {
+    spyOn(component.router, 'navigateByUrl').and.callThrough();
+    const cancelButton = await harness.cancelButton();
+
+    await cancelButton.click();
+
+    expect(component.router.navigateByUrl).toHaveBeenCalledWith('/notes');
   });
 });
